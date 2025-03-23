5b564ffe15676370411273f43116764c58f3f365
Mananans <guswls0477@gmail.com>
2025-03-23 15:03:55 +0900

    250323

---
+++ Assets/9999.TestDatabase/Object/Users/UserData.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class UserData 
+{
+    public string UserID;
+    public string UserPW;
+    public string UID;
+    public List<UserBattleInfo> BattleInfos;
+    public UserCannon CannonInfos;
+}
